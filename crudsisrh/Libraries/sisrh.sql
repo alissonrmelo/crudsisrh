8d691c9a9730b2316a235ad73264095fdfe59b61
Rodrigo <Rodrigo@DESKTOP-2NAC3ST>
2023-03-03 09:48:53 -0300

    Correção sql.

--- crudsisrh/Libraries/sisrh.sql
+++ crudsisrh/Libraries/sisrh.sql
@@ -1,42 +1,24 @@
---
--- Database: `sisrh`
---
+CREATE DATABASE IF NOT EXISTS `sisrh` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `sisrh`;
 
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `administradores`
---
-
-CREATE TABLE `administradores` (
+DROP TABLE IF EXISTS `administradores`;
+CREATE TABLE IF NOT EXISTS `administradores` (
   `login` varchar(12) NOT NULL,
   `password` varchar(8) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Extraindo dados da tabela `administradores`
---
-
 INSERT INTO `administradores` (`login`, `password`) VALUES
 ('Valmir', '12345678');
 
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `funcionarios`
---
-
-CREATE TABLE `funcionarios` (
-  `matricula` int(4) NOT NULL COMMENT 'Matrícula',
+DROP TABLE IF EXISTS `funcionarios`;
+CREATE TABLE IF NOT EXISTS `funcionarios` (
+  `matricula` int(4) NOT NULL AUTO_INCREMENT COMMENT 'Matrícula',
   `nome` varchar(50) NOT NULL,
   `rg` varchar(10) DEFAULT NULL,
   `cpf` varchar(14) DEFAULT NULL,
-  `telefoneContato` varchar(14) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Extraindo dados da tabela `funcionarios`
---
+  `telefoneContato` varchar(14) DEFAULT NULL,
+  PRIMARY KEY (`matricula`)
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8;
 
 INSERT INTO `funcionarios` (`matricula`, `nome`, `rg`, `cpf`, `telefoneContato`) VALUES
 (29, 'Alisson Rodrigo de Santos', '1682879', '837.662.601/97', '(61)98250.6665'),
@@ -45,61 +27,23 @@ INSERT INTO `funcionarios` (`matricula`, `nome`, `rg`, `cpf`, `telefoneContato`)
 (32, 'Valmir Ribeiro Carvalho', '55555555', '837.662.601/97', '(61)98250.6665'),
 (33, 'Valmir Ribeiro Carvalho', '1682898', '837.662.601/97', '(61)98250.6665');
 
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `usuarios`
---
-
-CREATE TABLE `usuarios` (
-  `id` int(10) NOT NULL,
+DROP TABLE IF EXISTS `usuarios`;
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
   `nome` varchar(100) CHARACTER SET latin1 NOT NULL,
   `sexo` varchar(100) CHARACTER SET latin1 NOT NULL,
   `dtNascimento` varchar(100) CHARACTER SET latin1 NOT NULL,
   `cargo` varchar(100) CHARACTER SET latin1 NOT NULL,
   `lotacao` varchar(100) CHARACTER SET latin1 NOT NULL,
   `email` varchar(100) CHARACTER SET latin1 NOT NULL,
-  `telefone` varchar(100) CHARACTER SET latin1 NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Extraindo dados da tabela `usuarios`
---
+  `telefone` varchar(100) CHARACTER SET latin1 NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
 INSERT INTO `usuarios` (`id`, `nome`, `sexo`, `dtNascimento`, `cargo`, `lotacao`, `email`, `telefone`) VALUES
 (8, 'Daniel Ramos', 'Masculino', '01/12/2010', 'Desenvolverdor Junor', 'SEUSO', 'alisson.rodrigo@gmail.com', '(61) 98250-6665'),
 (9, 'Alisson Rodrigo de Melo', 'Masculino', '23/03/1978', 'Desenvolvedor Senior', 'COARH', 'alisson.rodrigo@gmail.com', '(61) 98250-6665');
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `funcionarios`
---
-ALTER TABLE `funcionarios`
-  ADD PRIMARY KEY (`matricula`);
-
---
--- Indexes for table `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `funcionarios`
---
-ALTER TABLE `funcionarios`
-  MODIFY `matricula` int(4) NOT NULL AUTO_INCREMENT COMMENT 'Matrícula', AUTO_INCREMENT=34;
---
--- AUTO_INCREMENT for table `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
